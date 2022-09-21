e371d9b75b983c6f4ebfb36ce47aecdba0c08744
Maiara Lange <maiaralange@microsoft.com>
2022-09-21 17:22:23 -0300

    Creating header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Header } from '../components/Header';
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1>Hello world!</h1>
+      <Header />
     </>
   );
 }
