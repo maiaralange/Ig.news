b1216cb575ec9a9b1a1af632a80fad24fa5b9392
Maiara Lange <maiaralange@microsoft.com>
2022-09-21 16:22:00 -0300

    Including sass

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:ital,wght@0,400;0,700;1,900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
