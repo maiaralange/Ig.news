5497ab6d4a44bd3ea4a3a7324655313c1652371e
Maiara Lange <maiaralange@microsoft.com>
2022-09-22 16:19:43 -0300

    Saving logged user into db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,10 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { fauna } from '../../../services/fauna';
 
-export const authOptions = {
+export default NextAuth({
+  secret: process.env.SIGNING_KEY,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
@@ -12,7 +15,25 @@ export const authOptions = {
         }
       }
     })
-  ]
-};
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
 
-export default NextAuth(authOptions);
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
+});
