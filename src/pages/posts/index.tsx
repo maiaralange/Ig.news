64f665a5c384c32026c1f1e33bf4fcf5f84869c3
Maiara Lange <maiaralange@microsoft.com>
2022-09-23 17:58:50 -0300

    Updating id to key

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -24,7 +24,7 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a href="#" id={post.slug}>
+            <a href="#" key={post.slug}>
               <time>{post.updatedAt}</time>
               <strong>{post.title}</strong>
               <p>{post.summary}</p>
